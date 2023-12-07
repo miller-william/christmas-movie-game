2f03da52765a4c3b6ef745d6d7281a2aabc160f3
William Miller <wejmiller@googlemail.com>
2023-12-07 14:47:59 +0000

    Prepare app for Heroku deployment

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # christmas-movie-game
+
+A simple Flask web app to play higher or lower on IMDB ratings on Christmas films.
---
+++ app.py
@@ -0,0 +1,48 @@
+from flask import Flask, render_template, request, redirect, url_for
+import pandas as pd
+import random
+
+app = Flask(__name__)
+
+# Load your dataset
+full_df = pd.read_csv('christmas_movies.csv')
+reduced_df = full_df.dropna()
+
+def pick_movies(df):
+    return df.sample(2)
+
+@app.route('/')
+def index():
+    return render_template('index.html')
+
+@app.route('/select_mode', methods=['GET', 'POST'])
+def select_mode():
+    if request.method == 'POST':
+        game_mode = request.form['game_mode']
+        return redirect(url_for('play', mode=game_mode))
+    return render_template('select_mode.html')
+
+
+@app.route('/play', methods=['GET', 'POST'])
+def play():
+    game_mode = request.args.get('mode', 'full')
+    df = reduced_df if game_mode == 'reduced' else full_df
+
+    if request.method == 'POST':
+        user_guess = request.form['guess']
+        movie1_title = request.form['movie1']
+        movie2_title = request.form['movie2']
+        movie1 = df[df['title'] == movie1_title].iloc[0]
+        movie2 = df[df['title'] == movie2_title].iloc[0]
+
+        correct = ((movie2['imdb_rating'] >= movie1['imdb_rating'] and user_guess == 'movie2_higher') or
+                   (movie2['imdb_rating'] <= movie1['imdb_rating'] and user_guess == 'movie1_higher'))
+        
+        return render_template('result.html', movie1=movie1, movie2=movie2, correct=correct)
+
+    movies = pick_movies(df)
+    return render_template('play.html', movie1=movies.iloc[0].to_dict(), movie2=movies.iloc[1].to_dict())
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
